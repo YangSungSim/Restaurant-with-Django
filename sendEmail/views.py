8841a4e4540e5c5503f2ff015ad7676725c8c854
yangsungsim227 <yangsungsim227@gmail.com>
2020-01-21 11:33:02 +0900

    finished
    finished

--- sendEmail/views.py
+++ sendEmail/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
+from django.http import HttpResponse,HttpResponseRedirect
+from django.urls import reverse
+from django.shortcuts import render
+import sys
+import os
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.text import MIMENonMultipart
+from django.core.mail import send_mail, EmailMessage
+from django.template.loader import render_to_string
+
+sys.path.append('D:/Django/RestaurantShare/')
+from shareRes.models import *
+
 
 # Create your views here.
+def sendEmail(request):
+    try:
+        checked_res_list = request.POST.getlist('checks')
+        inputReceiver = request.POST['inputReceiver']
+        inputTitle = request.POST['inputTitle']
+        inputContent = request.POST['inputContent']
+        restaurants = []
+        for checked_res_id in checked_res_list:
+            restaurants.append( Restaurant.objects.get(id = checked_res_id))
+
+        content = {'inputContent':inputContent,'restaurants':restaurants}
+
+        msg_html = render_to_string('sendEmail/email_format.html',content)
+
+        msg = EmailMessage(subject = inputTitle,body=msg_html,from_email='fpahsk98@gmail.com',bcc=inputReceiver.split(','))
+        msg.content_subtype = 'html'
+
+        msg.send()
+        return render(request,'sendEmail/sendSuccess.html')
+    except:
+        return render(request,'sendEmail/sendFail.html')
+
--- shareRes/models.py
+++ shareRes/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    category_name = models.CharField(max_length=100)
+
+class Restaurant(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.SET_DEFAULT,default=3)
+    # 참조되는 요소가 삭제될 때 참조값을 설정해둔 default 값으로 설정한다. 레스토랑 요소가 참조하는 카테고리가 삭제될 때 자동으로 id값이 3인
+    # 카테고리를 참조한다.
+    restaurant_name = models.CharField(max_length= 100)
+    restaurant_link = models.CharField(max_length= 500)
+    restaurant_content = models.TextField()
+    restaurant_keyword = models.CharField(max_length= 50)
